e828f48409c797a56a1ff24f5648f5b1cfd68067
Shay <shaybishay@gmail.com>
2024-08-03 03:50:54 +0300

    close navbar when clicking outside navbar using screenSize hook. style change

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Education from "./components/Education";
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
+import { useState } from "react";
 
 const App = () => {
   useTitle("Shay's CV Webpage");
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Link } from "react-scroll";
 import menu_icon from "../assets/menu-icon.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -15,13 +15,51 @@ const Navbar = () => {
     });
   }, []);
 
-  const [mobileMenu, setMobileMenu] = useState(false);
-  const toggleMenu = () => {
-    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  const isMobile = useScreenSize();
+  const pcOffset = {
+    experience: -160,
+    projects: -180,
+    education: -160,
+    skills: -180,
+    contact: -300,
+  };
+  const phoneOffset = {
+    experience: -110,
+    projects: -90,
+    education: -80,
+    skills: -80,
+    contact: -240,
   };
 
+  const { experience, projects, education, skills, contact } = isMobile
+    ? phoneOffset
+    : pcOffset;
+
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    setMobileMenu(!mobileMenu);
+  };
+
+  const handleClickOutside = (event) => {
+    if (navbar.current && !navbar.current.contains(event.target)) {
+      setMobileMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const navbar = useRef();
+
   return (
-    <nav className={`container navbar ${sticky ? "dark-nav" : ""}`}>
+    <nav
+      className={`container navbar ${sticky ? "dark-nav" : ""}`}
+      ref={navbar}
+    >
       <img src={logo} alt="" className="logo" />
       <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
         <li>
@@ -30,22 +68,27 @@ const Navbar = () => {
           </Link>
         </li>
         <li>
-          <Link to="experience" smooth={true} offset={-160} duration={500}>
+          <Link
+            to="experience"
+            smooth={true}
+            offset={experience}
+            duration={500}
+          >
             Experience
           </Link>
         </li>
         <li>
-          <Link to="projects" smooth={true} offset={-180} duration={500}>
+          <Link to="projects" smooth={true} offset={projects} duration={500}>
             Projects
           </Link>
         </li>
         <li>
-          <Link to="education" smooth={true} offset={-160} duration={500}>
+          <Link to="education" smooth={true} offset={education} duration={500}>
             Education
           </Link>
         </li>
         <li>
-          <Link to="skills" smooth={true} offset={-180} duration={500}>
+          <Link to="skills" smooth={true} offset={skills} duration={500}>
             Skills
           </Link>
         </li>
@@ -53,7 +96,7 @@ const Navbar = () => {
           <Link
             to="contact"
             smooth={true}
-            offset={-260}
+            offset={contact}
             duration={500}
             className="btn"
           >
