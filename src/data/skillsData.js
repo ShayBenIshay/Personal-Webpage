3170b67cf7e90eede5c25ee22c5d9fadd535cd35
Shay <shaybishay@gmail.com>
2024-09-18 11:43:18 +0300

    Skills updated: NextJS

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,12 +1,13 @@
 export const skillsData = {
-  BackEndDevelopment: ["Node.js", "Express.js framework"],
-  FrontEndDevelopment: ["React.js", "Redux.js"],
-  VersionControl: ["Git"],
-  StackAndTechnologies: [
-    "MongoDB",
-    "Bitbucket",
-    "Jenkins",
-    "OpenShift Container Platform (OCP)",
+  BackEndDevelopment: [
+    "Node.js",
+    "Express.js",
+    "Java (Spring Framework)",
+    "NextJS",
   ],
+  FrontEndDevelopment: ["React.js", "Redux.js", "NextJS"],
+  CodeManagement: ["Git", "Bitbucket"],
+  DataBase: ["MongoDB"],
+  // Tools: ["Jenkins", "OpenShift Container Platform (OCP)"],
   //Unpolished skills: Python, Java, C, C++
 };
